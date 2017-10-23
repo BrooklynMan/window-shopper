3d08883509210d8cce52d28dc5c94376e14f0c57
adam mcbride <adam.mcbride@gmail.com>
2017-10-23 16:18:55 -0400

    wronte the logic function for calculating the can i buy it? function. learned about unit testing and the value of running them. also ran a couple of the logic function.

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -18,7 +18,7 @@ class MainVC: UIViewController {
         super.viewDidLoad()
         
         let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
-        calcBtn.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
         calcBtn.setTitle("Calculate", for: .normal)
         calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
         calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,16 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Adam McBride on 10/23/17.
+//  Copyright © 2017 Adam McBride. All rights reserved.
+//
+
+import Foundation
+
+
+class Wage {
+    class func getHours( forWage wage: Double, andprice price: Double) -> Int {
+        return Int(ceil(price / wage))
+    }
+}
